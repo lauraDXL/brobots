5eccaa14de989dc02eed9326f73e3d7da472a299
lauraDXL <laurasimion89@gmail.com>
2021-10-29 17:12:16 +0300

    new team page

--- src/static_data/team.js
+++ src/static_data/team.js
@@ -7,22 +7,7 @@ import stefi from '../assets/team/Stefania.jpg';
 import bogdan from '../assets/team/bogdan.png';
 import razvan from '../assets/team/Razvan.png';
 import ditoiu from '../assets/team/Ditoiu.png';
-import calin from '../assets/team/Calin.png';
-import luca from '../assets/team/Luca.png';
-import lara from '../assets/team/Lara.png'; 
-import alexia from '../assets/team/Alexia.png'; 
-import bianca from '../assets/team/Bianca.png'; 
-import abi from '../assets/team/abi.png'; 
-import dia from '../assets/team/dia.png'; 
-import arturo from '../assets/team/arturo.png'; 
-import crina from '../assets/team/Crina.png'; 
-import octa from '../assets/team/Octavian.png';
-import maria from '../assets/team/Maria.png';
-import mihai from '../assets/team/Mihai.png';
-import alexandros from '../assets/team/Alexandros.png';
-import mitran from '../assets/team/Mitran.png';
-import costin from '../assets/team/Costin.png';
-import fabio from '../assets/team/Fabio.png';
+
 export const team = [
 
   {
@@ -33,7 +18,8 @@ export const team = [
     description:
       "Sunt pasionat de IT, design grafic și citit. \n Mă consider o fire ambițioasă, perseverentă și prietenoasă. \nÎmi place să ajut oamenii în orice mod posibil și încerc să fac lucruri cât mai de impact pentru comunitate.",
     description_en:
-      'I am passionate about IT, graphic design and reading.\n I consider myself ambitious, persevering and friendly.\n I like to help people in any way I can and I try to do things that are as impactful as possible for the community.'
+      'I am passionate about IT, graphic design and reading.\n I consider myself ambitious, persevering and friendly.\n I like to help people in any way I can and I try to do things that are as impactful as possible for the community.',
+      
   },
 
   {
@@ -43,7 +29,8 @@ export const team = [
     instagram: 'https://www.instagram.com/stepi_on_me/',
     description_en:"I love psychology, graphic design and nature. In my free time, I enjoy helping people, getting involved in the community as much as possible, reading, drawing, and playing video games. I am quite persevering and motivated in what I do.",
     description: 
-    "Ador psihologia, graphic design-ul și natura.  În timpul meu liber, îmi place să ajut lumea, să mă implic în comunitate cât se poate de mult, să citesc, să desenez și să mă joc jocuri video. Sunt destul de perseverentă și motivată în ceea ce fac."
+    "Ador psihologia, graphic design-ul și natura.  În timpul meu liber, îmi place să ajut lumea, să mă implic în comunitate cât se poate de mult, să citesc, să desenez și să mă joc jocuri video. Sunt destul de perseverentă și motivată în ceea ce fac.",
+    
   },
 
   {
@@ -56,7 +43,8 @@ export const team = [
     description_en:
       'I am passionate about traveling, music and I like meeting new people. I like to get involved in as many volunteering projects as possible. I am a sociable person and I like to spend my time learning as many new things as possible from various fields',
     description:
-      'Sunt pasioanată de călătorit, muzică și îmi place să cunosc persoane noi. Îmi place să mă implic în cât mai multe proiecte de voluntariat. Sunt o persoană sociabilă și îmi place să îmi petrec timpul aflând cât mai multe lucruri noi din diverse domenii'
+      'Sunt pasioanată de călătorit, muzică și îmi place să cunosc persoane noi. Îmi place să mă implic în cât mai multe proiecte de voluntariat. Sunt o persoană sociabilă și îmi place să îmi petrec timpul aflând cât mai multe lucruri noi din diverse domenii',
+      
   },
  
 
@@ -68,6 +56,7 @@ export const team = [
       "I am cheerful and I like to meet as many people as possible. I am passionate about video science, drawing, philosophy and computer games, but from time to time I like to practice playing the guitar. I also like to give speeches and presentations.",
     description:
       'Sunt o fire veselă și îmi place să cunosc cât mai mulți oameni. Sunt pasionat de informatică, desenat, filosofie și jocuri pe calculator dar din când în când îmi place să exersez cântatul la chitară. De asemenea, îmi place să țin discursuri și prezentări',
+      
   },
   
  
@@ -79,7 +68,8 @@ export const team = [
     description_en:
       'I am thoughtful and rational. In my free time I like to go to the gym, go outside and play on the computer. My main passion, which I want to focus on, is programming.',
     description:
-      'Sunt o fire gânditoare și rațională. În timpul liber îmi place să merg la sală, să ies pe afară și să mă joc pe calculator. Pasiunea mea principală, de viitor și pe care vreau să mă axez în continuare, este programarea.'
+      'Sunt o fire gânditoare și rațională. În timpul liber îmi place să merg la sală, să ies pe afară și să mă joc pe calculator. Pasiunea mea principală, de viitor și pe care vreau să mă axez în continuare, este programarea.',
+      
   },
 
   {
@@ -90,7 +80,8 @@ export const team = [
     description_en:
       "I am passionate about 3D design and assembly. I like nature, to be the soul of the party and to help people. I consider myself a calm, funny person and someone you can count on",
     description:
-      'Sunt pasionat de proiectare 3D și asamblare.  Îmi place natura, să fiu sufletul petrecerii și să ajut lumea.  Mă consider o fire calmă, amuzantă și pe care te poți baza'
+      'Sunt pasionat de proiectare 3D și asamblare.  Îmi place natura, să fiu sufletul petrecerii și să ajut lumea.  Mă consider o fire calmă, amuzantă și pe care te poți baza',
+      
   },
   
   {
@@ -104,7 +95,8 @@ export const team = [
     description_en:
       "I am passionate about IT, 3D design and mechanics. I consider myself calm and friendly. I like learning new things, playing video games and listening to music.",
     description:
-      'Sunt pasionat de IT, proiectare 3D și mecanică. Mă consider o fire calmă și prietenoasă. Îmi place să învăț lucruri noi, să mă joc jocuri video și să ascult muzică.'
+      'Sunt pasionat de IT, proiectare 3D și mecanică. Mă consider o fire calmă și prietenoasă. Îmi place să învăț lucruri noi, să mă joc jocuri video și să ascult muzică.',
+      
   },
 
 
@@ -116,7 +108,8 @@ export const team = [
     description_en:
       "I like to find out as many new things as possible, meet many people and create unforgettable memories. I spend my time with many of my favorite activities, such as music, drawing, computer science and photography.",
     description:
-      'Îmi place să aflu cât mai multe lucruri noi, persoane și sa creez amintiri de neuitat. Îmi ocup timpul cu multe activități cele mai îndrăgite sunt muzica, desenul, informatica și fotografia.'
+      'Îmi place să aflu cât mai multe lucruri noi, persoane și sa creez amintiri de neuitat. Îmi ocup timpul cu multe activități cele mai îndrăgite sunt muzica, desenul, informatica și fotografia.',
+      
   },
 
   {
@@ -127,164 +120,8 @@ export const team = [
     description_en:
       "I am passionate about programming, video games and reading. I like to listen to a lot of music, my favorites being rock and EDM. I consider myself a calm, friendly and ambitious person.",
     description:
-      'Sunt pasionat de programare, jocuri pe calculator și citit. Îmi place să ascult multă muzică, preferata mea fiind rock-ul si EDM-ul. Mă consider o persoană calmă, prietenoasă și ambițioasă.'
+      'Sunt pasionat de programare, jocuri pe calculator și citit. Îmi place să ascult multă muzică, preferata mea fiind rock-ul si EDM-ul. Mă consider o persoană calmă, prietenoasă și ambițioasă.',
+      
   },
-
-  {
-    name: 'Ariciu Călin',
-    instagram: "https://instagram.com/ariciucalin?utm_medium=copy_link",
-    img: calin ,
-    description_en:
-      "My passions are cars, music, gaming and everything related to technology. I am interested in robotics because I consider it a field of the future, which is constantly evolving.",
-    description:
-      'Pasiunile mele sunt mașinile, muzica, gaming-ul și tot ce ține de tehnologie. Sunt interesat de robotică deoarece consider că este un domeniu de viitor, care este în continuă evoluție.'
-  },
- 
-  {
-    name: 'Tamaș Albert',
-    img: abi,
-    instagram: 'https://www.instagram.com/lo_wang._/',
-    description_en:
-      "Most of my passions would be music and video games, but at the same time I am very serious about my future and important work in general. I also like to travel and explore new things",
-    description:
-      'În mare parte pasiunile mele ar fi muzica și jocurile video, dar în același timp sunt foarte serios cu privire la viitorul meu și la munca importantă în general. De asemenea, îmi place să călătoresc și să explorez lucruri noi'
-  },
- 
-
-  {
-    name: 'Mihăilescu Lara',
-    img: lara ,
-    instagram: "https://www.instagram.com/steff_tifi/",
-    description_en:
-      'I am very ambitious and perfectionist, sometimes even stubborn. My favorite song is "Aristocats". In my free time I love to dance and play the piano.',
-    description:
-      'Sunt o fire foarte ambițioasă si perfecționistă, câteodată chiar încăpățânată. Melodia mea preferată este "Aristocats". În timpul liber ador să dansez și să cânt la pian.',
-  },
-
-  {
-    name: 'Sminticiuc Alexia',
-    img: alexia ,
-    
-    description_en:
-      "I am passionate about crypto, IT and reading.  I am defined by my ambition and perseverance. I like programming and design.",
-    description:
-      'Sunt pasionată de crypto, domeniul IT și cititul. Mă definesc ambiția si perseverența.  Îmi plac programarea și proiectarea.'
-  },
-
-  {
-    name: 'Andreescu Bianca',
-    img: bianca ,
-    facebook: 'https://web.facebook.com/profile.php?id=100009218352532 ',
-    instagram: 'https://www.instagram.com/byanka_nykoleta/',
-    description_en:
-      "I really like: music, math, photography and reading. I can describe myself as a cheerful, ambitious person, eager to learn new and creative things.",
-    description:
-      'Îmi plac foarte mult: muzica, matematica, fotografia și cititul.  Mă pot descrie ca fiind o fire veselă, ambițioasă, dornică de a învăța lucruri noi și creativă.'
-  },
-
-  {
-    name: 'Vlăduțu Luca',
-    img: luca ,
-    description_en:
-      "I like cars and motorsport of all kinds. I also like gaming and reading from time to time",
-    description:
-      'Îmi plac mașinile și motorsportul de toate felurile. De asemenea, îmi plac gaming-ul și cititul din când în când',
-  },
-
-  {
-    name: 'Diaconeasa Andrei',
-    img: dia ,
-    facebook: 'https://m.facebook.com/andrei.diaconeasa.925?ref=bookmarks',
-    instagram: 'https://www.instagram.com/andrei_ids/',
-    description_en:"I am a very curious and ambitious personI am passionate about video games, sports and musicI always like to learn new things."   ,
-     description:"Sunt o persoana foarte curioasă și ambitioasă.  Sunt pasionat de jocuri video, sport și muzică. Îmi place să învăț mereu lucruri noi. "
-  },
-
-  {
-    name: 'Ivența Caralicea Arturo',
-    img: arturo,
-    instagram: 'https://www.instagram.com/arturo.exe/',
-    description_en:
-      "I am creative, realist and sociable. I'm willing to learn new things. I am passionate about science, physics and computers.",
-    description:
-      'Sunt o fire creativă, realistă și sociabilă. Sunt dispus să învăț lucruri noi. Sunt pasionat de știință, fizică și calculatoare.'
-  },
-
-  {
-    name: 'Ghebu Crina',
-    img: crina,
-     description_en:
-      "I am ambitious and a little stubborn. In my free time I like to learn various choreographies and make sketches and drawings. I like to fight for what I want.",
-    description:
-      'Sunt o fire ambitioasă și puțin încăpățânată. În timpul liber îmi place să învăț diverse coregrafii și să realizez schițe și desene. Îmi place să lupt pentru ceea ce vreau.'
-  },
-
-  
-  {
-    name: 'Ciobanu Octavian',
-    img: octa,
-     description_en:
-      "I like helping people. I like the assembly, design and programming parts. I believe in a future where robots will help us.",
-    description:
-      'Îmi place să ajut oamenii. Îmi place partea de asamblare, proiectare și programare. Cred într-un viitor în care roboții ne vor ajuta.'
-  },
-
-  {
-    name: 'Tudor Maria',
-    instagram: "https://instagram.com/_.marya_28?r=nametag",
-    img: maria,
-     description_en:
-      "I'm creative.  I like to get involved in projects and meet new people.  I believe in people's potential.  I spend my free time with friends.",
-    description:
-      "Sunt o fire creativă.  Îmi place să mă implic în proiecte și să cunosc oameni noi. Cred în potențialul oamenilor.     Îmi petrec timpul liber cu prietenii."
-  },
-
-  
-  
-
-  {
-    name: 'Bulacu Mihai',
-    img: mihai,
-     description_en:
-      "I'm a nice boy, funny and smart. I'm passionate about robotics. I'm ambitious.",
-    description:
-      "Sunt un baiat de treaba, amuzant și isteț. Sunt pasionat de robotică. Sunt o fire ambițioasă."
-  },
-
-  {
-    name: 'Mariescu Alexandros',
-    img: alexandros,
-     description_en:
-      "I'm a good boy, hardworking and honest. I'm passionate about robotics. I'm ambitious.'",
-    description:' Sunt un baiat de treaba, harnic și onest. Sunt pasionat de robotica.  Sunt o fire ambițioasă.'
-  },
-
-  {
-    name: 'Mitran Alexandru',
-    img: mitran,
-     description:
-      "Îmi plac jocurile video, să ascult muzica și să  joc basket.  Am vrut să mă alătur echipei de robotică pentru a învăța lucruri noi și pentru a cunoaște oameni noi.",
-    description_en:' I like video games, listening to music and playing basketball. I wanted to join the robotics team to learn new things and meet new people.'
-  },
-
-  {
-    name: 'Bădiță Costin',
-    img: costin,
-    instagram:'https://www.instagram.com/costinbadita/',
-     description:
-      "Îmi place să învăț lucruri noi.   Sunt atent la detalii.  Respect părerile coechipierilor. Îmi place să lucrez fără oprire alături de ceilalți.",
-    description_en:' I like to learn new things.  I pay attention to details. I respect the opinions of my teammates. I like to work non-stop with others.'
-  },
-
-  {
-    name: 'Bădălan Fabio',
-    img: fabio,
-     description:
-      "Sunt o persoana foarte sociabilă și responsabila. Îmi place sa îmi pun ambitia și devotamentul in tot ceea ce fac și sa încerc lucruri cât mai noi și provocatoare.",
-    description_en:' I am a very sociable and responsible person. I like to put my ambition and devotion into everything I do and try new and challenging things.'
-  },
-
-
-
 
 ];
---
+++ src/static_data/volunteers.js
@@ -0,0 +1,188 @@
+import calin from '../assets/team/Calin.png';
+import luca from '../assets/team/Luca.png';
+import lara from '../assets/team/Lara.png'; 
+import alexia from '../assets/team/Alexia.png'; 
+import bianca from '../assets/team/Bianca.png'; 
+import abi from '../assets/team/abi.png'; 
+import dia from '../assets/team/dia.png'; 
+import arturo from '../assets/team/arturo.png'; 
+import crina from '../assets/team/Crina.png'; 
+import octa from '../assets/team/Octavian.png';
+import maria from '../assets/team/Maria.png';
+import mihai from '../assets/team/Mihai.png';
+import alexandros from '../assets/team/Alexandros.png';
+import mitran from '../assets/team/Mitran.png';
+import costin from '../assets/team/Costin.png';
+import fabio from '../assets/team/Fabio.png';
+
+export const volunteers = [
+    {
+        name: 'Ariciu Călin',
+        instagram: "https://instagram.com/ariciucalin?utm_medium=copy_link",
+        img: calin ,
+        description_en:
+          "My passions are cars, music, gaming and everything related to technology. I am interested in robotics because I consider it a field of the future, which is constantly evolving.",
+        description:
+          'Pasiunile mele sunt mașinile, muzica, gaming-ul și tot ce ține de tehnologie. Sunt interesat de robotică deoarece consider că este un domeniu de viitor, care este în continuă evoluție.',
+        
+      },
+     
+      {
+        name: 'Tamaș Albert',
+        img: abi,
+        instagram: 'https://www.instagram.com/lo_wang._/',
+        description_en:
+          "Most of my passions would be music and video games, but at the same time I am very serious about my future and important work in general. I also like to travel and explore new things",
+        description:
+          'În mare parte pasiunile mele ar fi muzica și jocurile video, dar în același timp sunt foarte serios cu privire la viitorul meu și la munca importantă în general. De asemenea, îmi place să călătoresc și să explorez lucruri noi',
+        
+      },
+     
+    
+      {
+        name: 'Mihăilescu Lara',
+        img: lara ,
+        instagram: "https://www.instagram.com/steff_tifi/",
+        description_en:
+          'I am very ambitious and perfectionist, sometimes even stubborn. My favorite song is "Aristocats". In my free time I love to dance and play the piano.',
+        description:
+          'Sunt o fire foarte ambițioasă si perfecționistă, câteodată chiar încăpățânată. Melodia mea preferată este "Aristocats". În timpul liber ador să dansez și să cânt la pian.',
+        
+      },
+    
+      {
+        name: 'Sminticiuc Alexia',
+        img: alexia ,
+        
+        description_en:
+          "I am passionate about crypto, IT and reading.  I am defined by my ambition and perseverance. I like programming and design.",
+        description:
+          'Sunt pasionată de crypto, domeniul IT și cititul. Mă definesc ambiția si perseverența.  Îmi plac programarea și proiectarea.',
+        
+      },
+    
+      {
+        name: 'Andreescu Bianca',
+        img: bianca ,
+        facebook: 'https://web.facebook.com/profile.php?id=100009218352532 ',
+        instagram: 'https://www.instagram.com/byanka_nykoleta/',
+        description_en:
+          "I really like: music, math, photography and reading. I can describe myself as a cheerful, ambitious person, eager to learn new and creative things.",
+        description:
+          'Îmi plac foarte mult: muzica, matematica, fotografia și cititul.  Mă pot descrie ca fiind o fire veselă, ambițioasă, dornică de a învăța lucruri noi și creativă.',
+        
+      },
+    
+      {
+        name: 'Vlăduțu Luca',
+        img: luca ,
+        description_en:
+          "I like cars and motorsport of all kinds. I also like gaming and reading from time to time",
+        description:
+          'Îmi plac mașinile și motorsportul de toate felurile. De asemenea, îmi plac gaming-ul și cititul din când în când',
+        
+      },
+    
+      {
+        name: 'Diaconeasa Andrei',
+        img: dia ,
+        facebook: 'https://m.facebook.com/andrei.diaconeasa.925?ref=bookmarks',
+        instagram: 'https://www.instagram.com/andrei_ids/',
+        description_en:"I am a very curious and ambitious personI am passionate about video games, sports and musicI always like to learn new things."   ,
+         description:"Sunt o persoana foarte curioasă și ambitioasă.  Sunt pasionat de jocuri video, sport și muzică. Îmi place să învăț mereu lucruri noi. ",
+         
+      },
+    
+      {
+        name: 'Ivența Caralicea Arturo',
+        img: arturo,
+        instagram: 'https://www.instagram.com/arturo.exe/',
+        description_en:
+          "I am creative, realist and sociable. I'm willing to learn new things. I am passionate about science, physics and computers.",
+        description:
+          'Sunt o fire creativă, realistă și sociabilă. Sunt dispus să învăț lucruri noi. Sunt pasionat de știință, fizică și calculatoare.',
+        
+      },
+    
+      {
+        name: 'Ghebu Crina',
+        img: crina,
+         description_en:
+          "I am ambitious and a little stubborn. In my free time I like to learn various choreographies and make sketches and drawings. I like to fight for what I want.",
+        description:
+          'Sunt o fire ambitioasă și puțin încăpățânată. În timpul liber îmi place să învăț diverse coregrafii și să realizez schițe și desene. Îmi place să lupt pentru ceea ce vreau.',
+        
+      },
+    
+      
+      {
+        name: 'Ciobanu Octavian',
+        img: octa,
+         description_en:
+          "I like helping people. I like the assembly, design and programming parts. I believe in a future where robots will help us.",
+        description:
+          'Îmi place să ajut oamenii. Îmi place partea de asamblare, proiectare și programare. Cred într-un viitor în care roboții ne vor ajuta.',
+        
+      },
+    
+      {
+        name: 'Tudor Maria',
+        instagram: "https://instagram.com/_.marya_28?r=nametag",
+        img: maria,
+         description_en:
+          "I'm creative.  I like to get involved in projects and meet new people.  I believe in people's potential.  I spend my free time with friends.",
+        description:
+          "Sunt o fire creativă.  Îmi place să mă implic în proiecte și să cunosc oameni noi. Cred în potențialul oamenilor.     Îmi petrec timpul liber cu prietenii.",
+        
+      },
+    
+      
+      
+    
+      {
+        name: 'Bulacu Mihai',
+        img: mihai,
+         description_en:
+          "I'm a nice boy, funny and smart. I'm passionate about robotics. I'm ambitious.",
+        description:
+          "Sunt un baiat de treaba, amuzant și isteț. Sunt pasionat de robotică. Sunt o fire ambițioasă.",
+        
+      },
+    
+      {
+        name: 'Mariescu Alexandros',
+        img: alexandros,
+         description_en:
+          "I'm a good boy, hardworking and honest. I'm passionate about robotics. I'm ambitious.'",
+        description:' Sunt un baiat de treaba, harnic și onest. Sunt pasionat de robotica.  Sunt o fire ambițioasă.',
+        
+      },
+    
+      {
+        name: 'Mitran Alexandru',
+        img: mitran,
+         description:
+          "Îmi plac jocurile video, să ascult muzica și să  joc basket.  Am vrut să mă alătur echipei de robotică pentru a învăța lucruri noi și pentru a cunoaște oameni noi.",
+        description_en:' I like video games, listening to music and playing basketball. I wanted to join the robotics team to learn new things and meet new people.',
+        
+      },
+    
+      {
+        name: 'Bădiță Costin',
+        img: costin,
+        instagram:'https://www.instagram.com/costinbadita/',
+         description:
+          "Îmi place să învăț lucruri noi.   Sunt atent la detalii.  Respect părerile coechipierilor. Îmi place să lucrez fără oprire alături de ceilalți.",
+        description_en:' I like to learn new things.  I pay attention to details. I respect the opinions of my teammates. I like to work non-stop with others.',
+        
+      },
+    
+      {
+        name: 'Bădălan Fabio',
+        img: fabio,
+         description:
+          "Sunt o persoana foarte sociabilă și responsabila. Îmi place sa îmi pun ambitia și devotamentul in tot ceea ce fac și sa încerc lucruri cât mai noi și provocatoare.",
+        description_en:' I am a very sociable and responsible person. I like to put my ambition and devotion into everything I do and try new and challenging things.',
+        
+      },
+];
